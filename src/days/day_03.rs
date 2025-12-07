d8c0c295b08e47431484bdd141fae4bc88e661fe
Matthew O'Connell <mattgoconn@gmail.com>
2025-12-06 17:29:33 -0800

    Day 3 Part 2
    Accepted solution for Day 3 Part 2

--- src/days/day_03.rs
+++ src/days/day_03.rs
@@ -1,5 +1,7 @@
 use std::io::BufRead;
 
+use itertools::Itertools;
+
 use crate::puzzle::Puzzle;
 
 pub(crate) fn get_puzzle() -> crate::puzzle::Puzzle {
@@ -47,8 +49,27 @@ fn _part_one(input: impl BufRead) -> anyhow::Result<u32> {
     })
 }
 
-fn _part_two(_input: impl BufRead) -> anyhow::Result<usize> {
-    todo!()
+fn _part_two(input: impl BufRead) -> anyhow::Result<usize> {
+    input
+        .lines()
+        .map(|line| {
+            let mut line: Vec<_> = line?.chars().filter_map(|c| c.to_digit(10)).collect();
+            Ok(max_joltage(&mut line))
+        })
+        .sum()
+}
+
+fn max_joltage(bank: &mut Vec<u32>) -> usize {
+    while bank.len() > 12 {
+        let remove_idx = bank
+            .windows(2)
+            .find_position(|pred| pred[0] < pred[1])
+            .map(|x| x.0)
+            .unwrap_or(bank.len() - 1);
+        bank.remove(remove_idx);
+    }
+
+    bank.iter().fold(0, |acc, el| acc * 10 + *el as usize)
 }
 
 #[cfg(test)]
@@ -64,6 +85,13 @@ mod tests {
     fn test_part_one() {
         let expected = 357;
         let actual = _part_one(INPUT.as_bytes()).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let expected = 3121910778619;
+        let actual = _part_two(INPUT.as_bytes()).unwrap();
         assert_eq!(expected, actual);
     }
 }

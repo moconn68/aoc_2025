b48e8eeff188532f1aff3bb26a0544426bee5ec0
Matthew O'Connell <mattgoconn@gmail.com>
2025-12-08 00:10:29 -0800

    Day 4 Part 2
    Accepted solution for Day 4 Part 2.

--- src/days/day_04.rs
+++ src/days/day_04.rs
@@ -18,56 +18,18 @@ fn parse_input(input: impl BufRead) -> std::io::Result<Vec<Vec<Space>>> {
 
 fn part_one(input: Box<dyn BufRead>) -> anyhow::Result<crate::puzzle::Answer> {
     let matrix = parse_input(input)?;
-    let ans = _part_one(matrix);
+    let ans = _part_one(&matrix);
     Ok(Box::new(ans))
 }
 
 fn part_two(input: Box<dyn BufRead>) -> anyhow::Result<crate::puzzle::Answer> {
-    let ans = _part_two(input)?;
+    let mut matrix = parse_input(input)?;
+    let ans = _part_two(&mut matrix);
     Ok(Box::new(ans))
 }
 
-fn _part_one<T, U>(input: T) -> usize
-where
-    T: AsRef<[U]>,
-    U: AsRef<[Space]>,
-{
-    let input = input.as_ref();
-    input
-        .iter()
-        .enumerate()
-        .flat_map(|(ridx, row)| {
-            row.as_ref()
-                .iter()
-                .enumerate()
-                .filter(move |(cidx, space)| {
-                    if !matches!(space, Space::Roll) {
-                        return false;
-                    }
-
-                    let adjacent_pos = [
-                        (ridx.checked_sub(1), cidx.checked_sub(1)),
-                        (ridx.checked_sub(1), Some(*cidx)),
-                        (ridx.checked_sub(1), Some(cidx + 1)),
-                        (Some(ridx), cidx.checked_sub(1)),
-                        (Some(ridx), Some(cidx + 1)),
-                        (Some(ridx + 1), cidx.checked_sub(1)),
-                        (Some(ridx + 1), Some(*cidx)),
-                        (Some(ridx + 1), Some(cidx + 1)),
-                    ]
-                    .into_iter()
-                    .filter_map(|(a, b)| a.zip(b));
-
-                    let adjacent_rolls = adjacent_pos.filter(|(r, c)| {
-                        let pos_space = input.get(*r).and_then(|r| r.as_ref().get(*c));
-
-                        pos_space.is_some_and(|space| space == &Space::Roll)
-                    });
-
-                    adjacent_rolls.count() < 4
-                })
-        })
-        .count()
+fn _part_one(input: &[Vec<Space>]) -> usize {
+    get_candidate_positions(input).count()
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -86,8 +48,63 @@ impl From<char> for Space {
     }
 }
 
-fn _part_two(_input: impl BufRead) -> anyhow::Result<usize> {
-    todo!()
+fn _part_two(input: &mut [Vec<Space>]) -> usize {
+    let mut total = 0;
+    while let to_remove = get_candidate_positions(input).collect::<Vec<_>>()
+        && !to_remove.is_empty()
+    {
+        total += to_remove.len();
+        for (ridx, cidx) in to_remove {
+            input[ridx][cidx] = Space::Empty;
+        }
+    }
+    total
+}
+
+/// For a given matix state, obtain the positions which are candidates for removal
+/// per the problem's definition (there are fewer than four rolls of paper in the eight adjacent positions).
+fn get_candidate_positions(input: &[Vec<Space>]) -> impl Iterator<Item = (usize, usize)> {
+    input.iter().enumerate().flat_map(move |(ridx, row)| {
+        row.iter().enumerate().filter_map(move |(cidx, space)| {
+            if !matches!(space, Space::Roll) {
+                return None;
+            }
+
+            let adjacent_pos = get_adjacent_positions_checked(ridx, cidx);
+
+            let adjacent_rolls = adjacent_pos.filter(|(r, c)| {
+                let pos_space = input.get(*r).and_then(|r| r.get(*c));
+
+                pos_space.is_some_and(|space| space == &Space::Roll)
+            });
+
+            if adjacent_rolls.count() < 4 {
+                Some((ridx, cidx))
+            } else {
+                None
+            }
+        })
+    })
+}
+
+/// In a 2D matrix, for any given position (ridx, cidx), return the indices in the matrix which are
+/// adjacent to it. This uses checked subtration to filter out any invalid negative indices.
+fn get_adjacent_positions_checked(
+    ridx: usize,
+    cidx: usize,
+) -> impl Iterator<Item = (usize, usize)> {
+    [
+        (ridx.checked_sub(1), cidx.checked_sub(1)),
+        (ridx.checked_sub(1), Some(cidx)),
+        (ridx.checked_sub(1), Some(cidx + 1)),
+        (Some(ridx), cidx.checked_sub(1)),
+        (Some(ridx), Some(cidx + 1)),
+        (Some(ridx + 1), cidx.checked_sub(1)),
+        (Some(ridx + 1), Some(cidx)),
+        (Some(ridx + 1), Some(cidx + 1)),
+    ]
+    .into_iter()
+    .filter_map(|(a, b)| a.zip(b))
 }
 
 #[cfg(test)]
@@ -110,6 +127,14 @@ mod tests {
         let expected = 13;
         let input_vec = parse_input(INPUT.as_bytes()).unwrap();
         let actual = _part_one(&input_vec);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let expected = 43;
+        let mut input_vec = parse_input(INPUT.as_bytes()).unwrap();
+        let actual = _part_two(&mut input_vec);
         assert_eq!(expected, actual);
     }
 }
